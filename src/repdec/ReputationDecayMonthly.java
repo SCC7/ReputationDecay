76e30ef271c7d1d473510e7d2a114b2b8067e129
SCC <84974831+SCC7@users.noreply.github.com>
2021-05-29 22:37:33 +0200

    2.1: month timeout
    Rep now doesn't change if there was a reputation changing event in the previous month.

--- src/repdec/ReputationDecayMonthly.java
+++ src/repdec/ReputationDecayMonthly.java
@@ -8,7 +8,6 @@ import com.fs.starfarer.api.campaign.RepLevel;
 import org.apache.log4j.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
-
 import java.io.IOException;
 
 
@@ -18,6 +17,8 @@ public class ReputationDecayMonthly implements EconomyTickListener {
     boolean repThresholdCooperative = true;
     boolean repThresholdVengeful = true;
     //float safetyMargin = 0.01f;
+    float reputationLastMonth;
+    boolean hasRepChangedSinceLastMonth;
 
     public static Logger log = Global.getLogger(ReputationDecayMonthly.class);
 
@@ -45,29 +46,34 @@ public class ReputationDecayMonthly implements EconomyTickListener {
     @Override
     public void reportEconomyMonthEnd() {
         for (FactionAPI faction : Global.getSector().getAllFactions()) {
-            if (repThresholdCooperative) {
-                if(faction.getRelationship(Factions.PLAYER) >= RepLevel.COOPERATIVE.getMin() + 0.01f
-                && faction.getRelationship(Factions.PLAYER) <= (RepLevel.COOPERATIVE.getMin() + decay)) {
-                    faction.setRelationship(Factions.PLAYER, RepLevel.COOPERATIVE.getMin() + 0.01f);
-                  continue;
+            if(faction.getRelationship(Factions.PLAYER) != reputationLastMonth) hasRepChangedSinceLastMonth = true;
+            if(hasRepChangedSinceLastMonth){
+                if (repThresholdCooperative) {
+                    if(faction.getRelationship(Factions.PLAYER) >= RepLevel.COOPERATIVE.getMin() + 0.01f
+                            && faction.getRelationship(Factions.PLAYER) <= (RepLevel.COOPERATIVE.getMin() + decay)) {
+                        faction.setRelationship(Factions.PLAYER, RepLevel.COOPERATIVE.getMin() + 0.01f);
+                        continue;
+                    }
                 }
-            }
-            if (repThresholdVengeful) {
-                if(faction.getRelationship(Factions.PLAYER) <= RepLevel.VENGEFUL.getMin() + 0.01f
-                && faction.getRelationship(Factions.PLAYER) >= (RepLevel.VENGEFUL.getMin() + decay)) {
-                    faction.setRelationship(Factions.PLAYER, RepLevel.VENGEFUL.getMin() + 0.01f);
+                if (repThresholdVengeful) {
+                    if(faction.getRelationship(Factions.PLAYER) <= RepLevel.VENGEFUL.getMin() + 0.01f
+                            && faction.getRelationship(Factions.PLAYER) >= (RepLevel.VENGEFUL.getMin() + decay)) {
+                        faction.setRelationship(Factions.PLAYER, RepLevel.VENGEFUL.getMin() + 0.01f);
+                        continue;
+                    }
+                }
+                if (faction.getRelationship(Factions.PLAYER) > decay) {
+                    faction.adjustRelationship(Factions.PLAYER, -decay);
                     continue;
                 }
+                if (faction.getRelationship(Factions.PLAYER) < -decay) {
+                    faction.adjustRelationship(Factions.PLAYER, decay);
+                    continue;
+                }
+                faction.setRelationship(Factions.PLAYER, 0);
             }
-            if (faction.getRelationship(Factions.PLAYER) > decay) {
-                faction.adjustRelationship(Factions.PLAYER, -decay);
-                continue;
-            }
-            if (faction.getRelationship(Factions.PLAYER) < -decay) {
-                faction.adjustRelationship(Factions.PLAYER, decay);
-                continue;
-            }
-            faction.setRelationship(Factions.PLAYER, 0);
+            reputationLastMonth = faction.getRelationship(Factions.PLAYER);
+            hasRepChangedSinceLastMonth = false;
         }
     }
 }
